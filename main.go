6e86023745025bdd6d45492b5c3238c02eb63e31
RaccoonCoins <168057848+RaccoonCoins@users.noreply.github.com>
2024-07-14 12:59:48 +0700

    Update main.go

--- main.go
+++ main.go
@@ -199,7 +199,7 @@ func main() {
 	if appPort == ":" {
 	        appPort = ":4000" // Cổng mặc định nếu không có biến môi trường
 	}
-	fmt.Printf("Port : %s \n", port)
+	fmt.Printf("Port : %s \n", appPort)
 	token := "0:2212fce3578671c9c4c8577a658a5c444d071dbeb9ce47b9850ef0047be9e342,0:948673a596fc6d2c3123dfff64d3231e4d0e16bfaac506d5ebdb09f32b3b9c65"
 	apiTon := "AF2BN4CF7W243KAAAAANNF3BE5Z2UDOVWVWGECV7BX3V43RJO6AUTQXPJJH23YDJFKKAHEY"
 	//go subscribeToTransactions(token, apiTon)
