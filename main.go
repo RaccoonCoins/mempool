fe12c98a154dc175b44836973fd1ca748fd51523
RaccoonCoins <168057848+RaccoonCoins@users.noreply.github.com>
2024-07-14 12:21:02 +0700

    Update main.go

--- main.go
+++ main.go
@@ -194,10 +194,7 @@ func ketnoidata(txname string) {
 
 }
 func main() {
-	port, err := os.Getenv("PORT")
-    	if err != nil {
-        	port = "4000"
-    	}
+
 	
 	token := "0:2212fce3578671c9c4c8577a658a5c444d071dbeb9ce47b9850ef0047be9e342,0:948673a596fc6d2c3123dfff64d3231e4d0e16bfaac506d5ebdb09f32b3b9c65"
 	apiTon := "AF2BN4CF7W243KAAAAANNF3BE5Z2UDOVWVWGECV7BX3V43RJO6AUTQXPJJH23YDJFKKAHEY"
@@ -212,6 +209,11 @@ func main() {
 
 	//gobot("asasas")
 	//go ketnoidata("haimeo", 9999999)
+	port := os.Getenv("PORT")
+    	if err != nil {
+        	port = "4000"
+    	}
+	fmt.Printf("Port : %s \n", port)
 	select {}
 
 }
